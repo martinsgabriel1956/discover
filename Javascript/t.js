049c9df5e5c0d6288ed3c4b22c1ef8d8e6e71636
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-01-18 14:31:46 -0300

    upadate files and created folders :tada:

--- Javascript/t.js
+++
@@ -1,9 +0,0 @@
-function myFunction(number) {
-
-  return number.toFixed(2);
-}
-
-console.log(myFunction(2.12397));
-console.log(myFunction(3.136));
-console.log(myFunction(1.12397));
-console.log(myFunction(26.1379));
